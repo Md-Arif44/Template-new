ef4b4892012d8a00507506916a3cdda148638b91
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-09 20:28:31 +0600

    Create tem.h

---
+++ tem.h
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h" 
+using namespace std;
+#ifndef DeBuG
+  #define dbg(...)  
+#endif
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define ff first 
+#define ss second 
+#define pb push_back 
+#define fora(cn) for(auto &x : (cn))
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define Rep(i,b) for(int i= 0 ;i<(b);++i)
+template<class T>using V=vector<T>; 
+using ll=long long; using pll = pair<ll,ll>;
+using pii=pair<int,int>;using vi=vector<int> ;
+using vl = vector<ll>  ;
+ 
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+   
+
+return 0 ;  
+}
