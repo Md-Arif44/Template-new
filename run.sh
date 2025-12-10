3fcc9e6c590579e667bae9dab6a8ff5720eb5b48
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-11 00:16:19 +0600

    Create run.sh for compiling and executing C++ files
    Added a shell script to compile and run C++ files using g++ and clang++.

---
+++ run.sh
@@ -0,0 +1,23 @@
+// compile and run  like run a where a is a cpp file
+// first open terminal bash then paste run() enter then source ~/.bashrc enter  
+run() {
+    code="$1"
+    g++ "${code}.cpp" -o "$code" -std=c++20 -g -DDeBuG \
+        -Wall -Wshadow -fsanitize=address,undefined \
+        && "./$code"
+}
+
+source ~/.bashrc
+
+/-------
+// for mac  same 
+run() {
+    code="$1"
+    clang++ "${code}.cpp" -o "$code" -std=c++20 -g -DDeBuG \
+        -Wall -Wshadow -fsanitize=address,undefined \
+        && "./$code"
+}
+
+source ~/.zshrc
+
+ 
