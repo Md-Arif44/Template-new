65050b2630204ebe10ef590505f76fb3d5c2b2e4
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-10 20:53:28 +0600

    Clean up comments in mint.h
    Removed author and license information from mint.h

---
+++ number-theory/mint.h
@@ -0,0 +1,19 @@
+/**
+* Description: Operators for modular arithmetic. You need to set {\tt mod} to
+*/
+
+const int mod=1e9+7;
+ struct mint{
+  ll x;
+  mint(ll x_=0):x(x_ % mod){if (x < 0) x += mod;}
+  mint power(ll e) const {
+      mint r = 1,b =*this;
+      for (;e;e>>=1,b=b*b) if(e&1)r=r*b;
+    return r;
+  }
+  mint inv(){return power(mod - 2);}
+  mint operator+(mint b) { return mint((x + b.x) % mod); }
+  mint operator-(mint b) { return mint((x - b.x + mod) % mod); }
+  mint operator*(mint b) { return mint((x * b.x) % mod); }
+  mint operator/(mint b) { return *this * b.power(mod - 2); }
+ };
