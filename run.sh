09e410ae7bf1869ca8deffa604729cdccc3893a2
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-11 12:39:42 +0600

    Clean up comments in run.sh
    Removed comments explaining how to use the run function.

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-// compile and run  like run a where a is a cpp file
-// first open terminal bash then paste run() enter then source ~/.bashrc enter  
 run() {
     code="$1"
     g++ "${code}.cpp" -o "$code" -std=c++20 -g -DDeBuG \
