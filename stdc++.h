5f64a76bb8b36ab143fcfe3ff7dd885b9c45f3a4
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-11 20:17:28 +0600

    Update stdc++.h

--- stdc++.h
+++ stdc++.h
@@ -1,46 +1,34 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cstdint>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <stack>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
-using namespace std; 
+/**
+ * Author: Arman Ferdous
+ * Date: 2025-06-16
+ * Description: 
+ */
+#include <bits/stdc++.h> /** keep-include */
+using namespace std;
 #define TT template <typename T
+
 TT,typename=void> struct cerrok:false_type {};
 TT> struct cerrok <T, void_t<decltype(cerr << declval<T>() )>> : true_type {};
 
 TT> constexpr void p1 (const T &x);
 TT, typename V> void p1(const pair<T, V> &x) {
-  cerr << "{"; p1(x.first); cerr << ","; 
+  cerr << "{"; p1(x.first); cerr << ", "; 
   p1(x.second); cerr << "}"; 
 }
 TT> constexpr void p1 (const T &x) {
   if constexpr (cerrok<T>::value) cerr << x;
   else { int f = 0; cerr << '{';
     for (auto &i: x) 
-      cerr << (f++ ? "," : ""), p1(i);
+      cerr << (f++ ? ", " : ""), p1(i);
     cerr << "}";
 } }
 void p2() { cerr << "]\n"; }
 TT, typename... V> void p2(T t, V... v) {
   p1(t);
-  if (sizeof...(v)) cerr << ",";
+  if (sizeof...(v)) cerr << ", ";
   p2(v...);
 }
+
 #ifdef DeBuG
-#define dbg(x...) {cerr <<"\t\e[93m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; p2(x); cerr << "\e[0m";}
+#define dbg(x...) {cerr << "\t\e[93m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; p2(x); cerr << "\e[0m";}
 #endif
