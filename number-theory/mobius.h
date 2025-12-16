5db25bc39e11caed93bd284faec7b7f5c4df8aa1
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 01:26:11 +0600

    Update mobius.h

--- number-theory/mobius.h
+++ number-theory/mobius.h
@@ -11,6 +11,11 @@ gcd(i, j) = k   (k ≥ 1)
 Σ_{d=1}^{min(⌊r1/k⌋, ⌊r2/k⌋)} μ(d) ·
 ( ⌊r1/(k·d)⌋ − ⌊(l1−1)/(k·d)⌋ ) ·
 ( ⌊r2/(k·d)⌋ − ⌊(l2−1)/(k·d)⌋ )
+--------------------------------
+Count pairs (i, j) in an array a[]
+gcd(a[i], a[j]) = k   (k ≥ 1)
+Let cnt[x] = number of elements in a divisible by x  
+Σ_{d=1}^{⌊Max/k⌋} μ(d) · cnt[d·k]^2
 */
 const int N = 5e5 + 9;
 int mob[N];
