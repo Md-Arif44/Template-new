56844b2280a6715a29ebb166509c2a64fb152bfa
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 20:21:59 +0600

    Update and rename zalgo.h to Zfunc.h

--- string/Zfunc.h
+++ string/Zfunc.h
@@ -1,8 +1,13 @@
 /**
+ * Author: chilli
+ * License: CC0
  * Description: z[i] computes the length of the longest common prefix of s[i:] and s,
  * except z[0] = 0. (abacaba -> 0010301)
  * Time: O(n)
+ * Status: stress-tested
  */
+#pragma once
+
 vi Z(const string& S) {
 	vi z(sz(S));
 	int l = -1, r = -1;
