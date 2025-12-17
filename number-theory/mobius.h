dcf722fc19df9cc31380fad8ec519ae89bad4495
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-18 02:11:41 +0600

    Update mobius.h

--- number-theory/mobius.h
+++ number-theory/mobius.h
@@ -1,29 +1,21 @@
-/**
+l/**
  * Author: 
  * Date:  
  * License:  
  * Source: 
  * Description: 
+ * Count x in [L, R], gcd(x, m) = k:
+ *   sum d|(m/k) mu[d] *
+ *   (R/(k*d) - (L-1)/(k*d))
+ * Count pairs (i, j), gcd(i, j) = k:
+ *   sum d<=min(r1/k, r2/k) mu[d] *
+ *   (r1/(k*d) - (l1-1)/(k*d)) *
+ *   (r2/(k*d) - (l2-1)/(k*d))
+ * Count pairs in array a[], gcd(a[i], a[j]) = k:
+ *   cnt[x] = # of elements divisible by x
+ *   sum d<=Max/k mu[d] * cnt[d*k]^2
  */
-/*
-Count x in [L, R]:
-gcd(x, m) = k   (k ≥ 1, k | m)
-1 ≤ L ≤ R
-Σ_{d | (m/k)} μ(d) ·
-( ⌊R/(k·d)⌋ − ⌊(L−1)/(k·d)⌋ )
 
-Count pairs (i, j) in ranges:
-gcd(i, j) = k   (k ≥ 1)
-1 ≤ l1 ≤ r1, 1 ≤ l2 ≤ r2
-Σ_{d=1}^{min(⌊r1/k⌋, ⌊r2/k⌋)} μ(d) ·
-( ⌊r1/(k·d)⌋ − ⌊(l1−1)/(k·d)⌋ ) ·
-( ⌊r2/(k·d)⌋ − ⌊(l2−1)/(k·d)⌋ )
-
-Count pairs (i, j) in an array a[]
-gcd(a[i], a[j]) = k   (k ≥ 1)
-Let cnt[x] = number of elements in a divisible by x  
-Σ_{d=1}^{⌊Max/k⌋} μ(d) · cnt[d·k]^2
-*/
 
 const int N = 5e5 + 9;
 int mob[N];
