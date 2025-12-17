a864e6e584a2d1ab1a4de517bfd752d2f8908e08
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 20:18:16 +0600

    Update and rename Kmp.h to KMP.h

--- string/KMP.h
+++ string/KMP.h
@@ -1,10 +1,15 @@
 /**
+ * Author: Johan Sannemo
+ * Date: 2016-12-15
+ * License: CC0
  * Description: pi[x] computes the length of the longest prefix of s that ends at x,
  * other than s[0...x] itself (abacaba -> 0010123).
  * Can be used to find all occurrences of a string.
  * Time: O(n)
  * Status: Tested on kattis:stringmatching
  */
+#pragma once
+
 vi pi(const string& s) {
 	vi p(sz(s));
 	rep(i,1,sz(s)) {
