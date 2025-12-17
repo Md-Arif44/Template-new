093ef45b267d1819bffd6369a7a4f1ae7d8978ae
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 20:29:18 +0600

    Update Lca.h

--- Graph/Lca.h
+++ Graph/Lca.h
@@ -1,24 +1,33 @@
 /**
- * Description: add Rmq.h  . O(1) lca(a,b) 
+ * Author: chilli, pajenegod
+ * Date: 2020-02-20
+ * License: CC0
+ * Source: Folklore
+ * Description: Data structure for computing lowest common ancestors in a tree
+ * (with 0 as root). C should be an adjacency list of the tree, either directed
+ * or undirected.
+ * Time: $O(N \log N + Q)$
+ * Status: stress-tested
  */
+#pragma once
 struct LCA {
-  int T = 0;
-  vi time, path, ret ,tour ;
-  RMQ<int> rmq;
-  LCA(vector<vi>& C) : time(sz(C)), rmq((dfs(C,0,-1), ret)) {}
-  void dfs(vector<vi>& C, int v, int par) {
-    time[v] = T++; tour.pb(v) ;
-    for (int y : C[v]) if (y != par) {
-      path.pb(v), ret.pb(time[v]);
-      dfs(C, y, v);
-    }
-    tour.pb(v) ;
-  }
- 
-  int lca(int a, int b) {
-    if (a == b) return a;
-    tie(a, b) = minmax(time[a], time[b]);
-    return path[rmq.query(a, b)];
-  }
-  //dist(a,b){return depth[a] + depth[b] - 2*depth[lca(a,b)];}
+	int T = 0;
+	vi time, path, ret;
+	RMQ<int> rmq;
+
+	LCA(vector<vi>& C) : time(sz(C)), rmq((dfs(C,0,-1), ret)) {}
+	void dfs(vector<vi>& C, int v, int par) {
+		time[v] = T++;
+		for (int y : C[v]) if (y != par) {
+			path.push_back(v), ret.push_back(time[v]);
+			dfs(C, y, v);
+		}
+	}
+
+	int lca(int a, int b) {
+		if (a == b) return a;
+		tie(a, b) = minmax(time[a], time[b]);
+		return path[rmq.query(a, b)];
+	}
+	//dist(a,b){return depth[a] + depth[b] - 2*depth[lca(a,b)];}
 };
