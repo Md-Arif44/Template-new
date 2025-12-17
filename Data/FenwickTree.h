70d60dc649ae7ba56d048bc2e7b76d366f7469ae
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 19:49:05 +0600

    Update and rename Bit.h to FenwickTree.h

--- Data/FenwickTree.h
+++ Data/FenwickTree.h
@@ -1,8 +1,16 @@
-/*
+/**
+ * Author: Lukas Polacek
+ * Date: 2009-10-30
+ * License: CC0
+ * Source: folklore/TopCoder
  * Description: \texttt{update(i,x): a[i] += x;}\\
  * \texttt{query(i): sum in [0, i);}\\
- * \texttt{lower\_bound(sum): min pos st sum of [0, pos] >= sum, returns n if all < sum, or -1 if empty sum.
+ * \texttt{lower\_bound(sum): min pos st sum of [0, pos] >= sum, returns n if all < sum, or -1 if empty sum.}
+ * Time: Both operations are $O(\log N)$.
+ * Status: Stress-tested
  */
+#pragma once
+
 struct FT {
   int n; V<ll> s;
   FT(int _n) : n(_n), s(_n) {}
