8d1fcf0062163e8da4653e9cbfbaba3913ad3908
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 19:51:19 +0600

    Update and rename Rmq.h to RMQ.h

--- Data/RMQ.h
+++ Data/RMQ.h
@@ -1,4 +1,8 @@
 /**
+ * Author: Johan Sannemo, pajenegod
+ * Date: 2015-02-06
+ * License: CC0
+ * Source: Folklore
  * Description: Range Minimum Queries on an array. Returns
  * min(V[a], V[a + 1], ... V[b - 1]) in constant time.
  * Usage:
@@ -7,6 +11,8 @@
  * Time: $O(|V| \log |V| + Q)$
  * Status: stress-tested
  */
+#pragma once
+
 template<class T>
 struct RMQ {
 	V<V<T>> jmp;
