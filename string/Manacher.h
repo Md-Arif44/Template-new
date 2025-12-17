b1863453f8df21d8032d4fd308220a6c17c74c35
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-12-17 20:20:05 +0600

    Update and rename manacher.h to Manacher.h

--- string/Manacher.h
+++ string/Manacher.h
@@ -1,8 +1,12 @@
 /**
+ * Author: User adamant on CodeForces
+ * Source: http://codeforces.com/blog/entry/12143
  * Description: For each position in a string, computes p[0][i] = half length of
  *  longest even palindrome around pos i, p[1][i] = longest odd (half rounded down).
  * Time: O(N)
+ * Status: Stress-tested
  */
+#pragma once
 
 array<vi, 2> manacher(const string& s) {
 	int n = sz(s);
